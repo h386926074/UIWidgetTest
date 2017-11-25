84d15e2ede4603539ce0f0bf134308dbc7fcdae8
Daniel <h386926074@163.com>
2017-11-26 00:38:03 +0800

    edittext

--- app/src/main/java/com/huangfeng/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/huangfeng/uiwidgettest/MainActivity.java
@@ -2,12 +2,40 @@ package com.huangfeng.uiwidgettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Button button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: ");
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+            {
+                EditText editTexts = (EditText) findViewById(R.id.edit_text);
+                editTexts.setText("I wirit some string");
+
+            }
+                break;
+            default:
+                break;
+        }
     }
 }
