841fe29542356dd9236cc863d0b79788b6fb9dcc
huangfeng <huangfeng@163.com>
2018-01-16 08:36:52 +0800

    constraintlayout

---
+++ app/src/main/java/com/huangfeng/uiwidgettest/percent_layout.java
@@ -0,0 +1,24 @@
+package com.huangfeng.uiwidgettest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class percent_layout extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent_layout);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public void add(View v){
+
+    }
+}
