04eb0237e7d40341e8e9b0dad9baa424dfe9e0da
huangfeng <huangfeng@163.com>
2018-02-05 18:17:39 +0800

    uibestpractice

--- app/src/main/java/com/huangfeng/uiwidgettest/UIBestPracticeActivity.java
+++ app/src/main/java/com/huangfeng/uiwidgettest/UIBestPracticeActivity.java
@@ -2,12 +2,57 @@ package com.huangfeng.uiwidgettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UIBestPracticeActivity extends AppCompatActivity {
+
+    private List<Msg> msgList = new ArrayList<>();
+
+    private EditText inputText;
+
+    private Button send;
+
+    private RecyclerView msgRecycleView;
+
+    private MsgAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.uibest_practice_layout);
+
+
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgRecycleView = (RecyclerView) findViewById(R.id.msg_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecycleView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+
+        
+
+
+    }
+
+
+    private void initMsgs(){
+
+        Msg msg1 = new Msg("Hello guy.",Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+
+        Msg msg2 = new Msg("Hello. Who is that?",Msg.TYPE_SENT);
+        msgList.add(msg2);
+
+        Msg msg3 = new Msg("This is Tom. Nice talking to you. ",Msg.TYPE_RECEIVED);
+        msgList.add(msg3);
+
+
     }
 }
