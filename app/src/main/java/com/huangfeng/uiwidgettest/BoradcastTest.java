c5f62f400e90aca2e9f5ec1efffc56d0d98cca33
huangfeng <huangfeng@163.com>
2018-02-22 11:59:57 +0800

    broadcast

--- app/src/main/java/com/huangfeng/uiwidgettest/BoradcastTest.java
+++ app/src/main/java/com/huangfeng/uiwidgettest/BoradcastTest.java
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -21,9 +24,12 @@ public class BoradcastTest extends AppCompatActivity {
         setContentView(R.layout.boradcast_test_layout);
 
         intentFilter = new IntentFilter();
+
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver,intentFilter);
+
+
 
     }
 
@@ -38,6 +44,17 @@ public class BoradcastTest extends AppCompatActivity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectionManager  = (ConnectivityManager)
+                    getSystemService(context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectionManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()){
+                Toast.makeText(context,"network is available",Toast.LENGTH_SHORT).show();
+            }else{
+
+                Toast.makeText(context,"network is unavailabel",Toast.LENGTH_SHORT).show();
+            }
+
+
             Toast.makeText(context,"network changes",Toast.LENGTH_SHORT).show();
         }
     }
